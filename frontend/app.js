7f3de54d3ff99e1b764e63ed277526acf1f7a64b
nogurenn <glennharveyliwanag@gmail.com>
2017-03-30 15:58:17 +0800

    Created reset() and hooked to Reset button. Added real-time sync

--- frontend/app.js
+++ frontend/app.js
@@ -4,6 +4,42 @@ angular.module('scheduleApp', ['firebase'])
 // create our main controller and get access to firebase
 .controller('mainController', function($scope, $firebase) {
 
-// our application code will go here
+    var ref = new Firebase("https://burning-torch-3627.firebaseio.com/days");
+    var fb = $firebase(ref);
 
+    // sync as object (three-way data binding)
+    var syncObject = fb.$asObject();
+    syncObject.$bindTo($scope, 'days');
+
+    // function to set the default data
+    $scope.reset = function() {
+        fb.$set({
+            monday: {
+                name: 'Monday',
+                slots: {
+                    0900: {
+                        time: '9:00am',
+                        booked: false
+                    },
+                    0110: {
+                        time: '11:00am',
+                        booked: false
+                    }
+                }
+            },
+            tuesday: {
+                name: 'Tuesday',
+                slots: {
+                    0900: {
+                        time: '9:00am',
+                        booked: false
+                    },
+                    0110: {
+                        time: '11:00am',
+                        booked: false
+                    }
+                }
+            }
+        });
+    };
 });
